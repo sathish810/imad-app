d467373573a290c8448fe3597fb151104f6f9f57
sathish810 <31757827+sathish810@users.noreply.github.com>
2017-09-12 11:43:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('articele.one',function(req,res) {
+app.get('article.one',function(req,res) {
     res.send('hi sathish');
-    app.get('articele.two',function(req,res) {
+    app.get('article.two',function(req,res) {
     res.send('welcome to ntptel');
 });
 });
